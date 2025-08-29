3b183b5417e1677d0d1b23a5ae3f063987c2ca6b
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-08-28 23:38:53 -0300

    update: created class Graph and uploaded .CSV test file

--- AIBinaryOption/main.py
+++ AIBinaryOption/main.py
@@ -3,11 +3,14 @@ from environment import Environment
 
 price_quote = []
 
-with open('', encoding='UTF-8') as file:
+with open('bcdata.sgs.10813.csv', encoding='UTF-8') as file:
 	lines = file.readlines()
 	for line in lines[1:]:
 		parts = line.strip().split(';')
 		value_str = parts[1].replace('"', '').replace(',', '.')
 		value = float(value_str)
 		price_quote.append(value)
+
+
+print(price_quote)
 
