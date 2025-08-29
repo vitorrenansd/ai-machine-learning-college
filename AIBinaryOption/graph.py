10d45881af37c492f2efa4a3a2aa05d8f88fa534
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-08-29 09:44:11 -0300

    refactor: created folder assets for all CSV files

--- AIBinaryOption/graph.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x = [i for i in range(len(price_quote))]
-y = list(reversed(price_quote))
-
-plt.plot(x, y)
-plt.xlabel('Time (day)')
-plt.ylabel('USD')
-plt.show()
