e7a2cf35a6dd709bd61a66b5f65c1d7354f11aa5
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-09-11 21:27:13 -0300

    refactor: room state now is set up in main.py

--- AIVacuum/environment.py
+++ AIVacuum/environment.py
@@ -1,6 +1,6 @@
 class Environment:
-	def __init__(self):
-		self.state = ["Dirty", "Clean", "X", "Dirty", "X", "Dirty", "Dirty", "Clean", "X"]
+	def __init__(self, state):
+		self.state = state
 
 	def show(self):
 		print(f'Environment: {self.state}')
