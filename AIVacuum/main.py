38c868ccbe81fc1cfe65cdae78a8e414a24e9b11
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-09-11 22:01:40 -0300

    refactor: now using get_room_size() from Environment class

--- AIVacuum/main.py
+++ AIVacuum/main.py
@@ -4,10 +4,10 @@ from environment import Environment
 environment = Environment(["Dirty", "Clean", "X", "Dirty", "X", "Dirty", "Dirty", "Clean", "X"])
 vacuum = Vacuum()
 
-print('Initial state')
-environment.show()
+room_size = environment.get_room_size()
+print(f'Initial state: {environment.show()}')
 
-for _ in range(9):
+for _ in range(room_size):
 	vacuum.action(environment)
 
 environment.show()
