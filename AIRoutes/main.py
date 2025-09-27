9be447ec1e7b2b6b1666005569d63c5fb4be489b
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-09-27 12:39:52 -0300

    update: added tests for Search class

--- AIRoutes/main.py
+++ AIRoutes/main.py
@@ -1,6 +1,8 @@
 from graph import Graph
+from search import Search
 
 g = Graph()
+s = Search()
 g.add_edge("A", "B")
 g.add_edge("A", "C")
 g.add_edge("B", "D")
@@ -9,3 +11,6 @@ g.add_edge("C", "F")
 g.add_edge("E", "F")
 
 g.print_graph()
+
+path = s.depth_first_search(g, "A", "D")
+print("Path found: ", path)
