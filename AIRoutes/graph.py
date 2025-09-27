dc1bb295a86b92e784858a8b21c959b3147be156
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-09-27 11:45:20 -0300

    update: created method print_graph() in Graph class

--- AIRoutes/graph.py
+++ AIRoutes/graph.py
@@ -1,10 +1,14 @@
 class Graph:
     def __init__(self):
         self.adjacency = {}
-            
+
     def add_edge(self, origin, destination):
         if origin not in self.adjacency:
             self.adjacency[origin] = []
         if destination not in self.adjacency:
             self.adjacency[destination] = []
         self.adjacency[origin].append(destination)
+
+    def print_graph(self):
+        for node, neighbors in self.adjacency.items():
+            print(f"{node} -> {', '.join(neighbors)}")
