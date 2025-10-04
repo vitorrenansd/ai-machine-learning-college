60988d9a2260b8cccccbb0d411ed4fa309aa83d0
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-04 09:45:41 -0300

    update: edit in main.py

--- AIRoutes/main.py
+++ AIRoutes/main.py
@@ -1,16 +1,6 @@
-from graph import Graph
-from search import Search
+from map import Map
+from depth_search import DepthSearch
 
-g = Graph()
-s = Search()
-g.add_edge("A", "B")
-g.add_edge("A", "C")
-g.add_edge("B", "D")
-g.add_edge("B", "E")
-g.add_edge("C", "F")
-g.add_edge("E", "F")
-
-g.print_graph()
-
-path = s.depth_first_search(g, "A", "D")
-print("Path found: ", path)
+map = Map()
+ds = DepthSearch(map.portoUniao, map.curitiba)
+ds.search()
