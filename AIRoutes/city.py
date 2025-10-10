dc88c997e5d0177e081e0b1a277e8d54308c449b
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-10 08:00:10 -0300

    update: added a __repr__ method

--- AIRoutes/city.py
+++ AIRoutes/city.py
@@ -1,9 +1,12 @@
 class City:
-    def __init__(self, name):
+    def __init__(self, name, goal_distance):
         self.name = name
         self.adjacents = []
         self.visited = False
-        self.goal_distance = None
+        self.goal_distance = goal_distance
+
+    def __repr__(self):
+        return f"{self.name} (h={self.goal_distance})"
 
     def add_adjacent_city(self, city):
         self.adjacents.append(city)
