07480c19eaf2c24eb6eab98b7734b1003a58d72d
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-16 09:42:14 -0300

    update: added distance and final_distance to Adjacent class

--- AIRoutes/adjacent.py
+++ AIRoutes/adjacent.py
@@ -1,3 +1,5 @@
 class Adjacent:
-    def __init__(self, city):
+    def __init__(self, city, distance):
         self.city = city
+        self.distance = distance
+        self.final_distance = self.distance + self.city.goal_distance
