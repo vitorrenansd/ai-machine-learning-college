a4b1cd4dad0cd1015aeb193e5d70625e323fea1c
Vitor Dias <vitorrenansouzadias@gmail.com>
2025-10-16 09:43:12 -0300

    testing AStar search

--- AIRoutes/main.py
+++ AIRoutes/main.py
@@ -1,10 +1,14 @@
 from map import Map
 from depth_search import DepthSearch
 from greedy_search import GreedySearch
+from astar_search import AStar
 
 map = Map()
-# ds = DepthSearch(map.portoUniao, map.curitiba)
-# ds.search()
+## ds = DepthSearch(map.portoUniao, map.curitiba)
+## ds.search()
 
-gs = GreedySearch(map.curitiba)
-gs.search(map.portoUniao)
+## gs = GreedySearch(map.curitiba)
+## gs.search(map.portoUniao)
+
+a_star = AStar(map.curitiba)
+a_star.search(map.portoUniao)
